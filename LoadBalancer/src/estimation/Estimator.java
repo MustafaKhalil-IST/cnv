8359b492ccfb698f5fb5b897a85d7f6140f2a387
MustafaKhalil-IST <mustafa.samir.khalil@tecnico.ulisboa.pt>
2020-05-20 13:25:21 +0100

    load balancer autoscale

--- LoadBalancer/src/estimation/Estimator.java
+++ LoadBalancer/src/estimation/Estimator.java
@@ -7,7 +7,7 @@ import java.util.Timer;
 public class Estimator implements Runnable {
 
     static Timer estimator = new Timer();
-    static int PERIOD = PropertiesReader.getInstance().getNumericalProperty("estimate.period.milliseconds");
+    static int PERIOD = PropertiesReader.getInstance().getNumericalProperty("estimate.period");
     @Override
     public void run() {
         estimator.schedule(new Estimation(), PERIOD, PERIOD);
