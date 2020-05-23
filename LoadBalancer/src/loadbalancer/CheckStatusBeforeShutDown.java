6d7fd45b42322c71987aee537163f0eee8cf136f
MustafaKhalil-IST <mustafa.samir.khalil@tecnico.ulisboa.pt>
2020-05-23 11:34:00 +0100

    load balancer fixes

--- LoadBalancer/src/loadbalancer/CheckStatusBeforeShutDown.java
+++ LoadBalancer/src/loadbalancer/CheckStatusBeforeShutDown.java
@@ -3,8 +3,10 @@ package src.loadbalancer;
 import com.amazonaws.services.ec2.AmazonEC2;
 
 import java.util.TimerTask;
+import java.util.logging.Logger;
 
 public class CheckStatusBeforeShutDown extends TimerTask {
+    private final static Logger logger = Logger.getLogger(CheckStatusBeforeShutDown.class.getName());
     AmazonEC2 client;
     InstanceProxy instance;
 
@@ -15,8 +17,10 @@ public class CheckStatusBeforeShutDown extends TimerTask {
 
     @Override
     public void run() {
+        logger.info("Instance " + instance.getAddress() + " has " + instance.currentRequests.size() + " requests .. ");
         // Shut down only if there is no requests left to be done by the instance
         if (instance.currentRequests.isEmpty()) {
+            logger.info("Shutting down instance " + instance.getAddress());
             instance.shutDown(client);
             InstancesManager.getSingleton().removeInstance(instance);
             this.cancel();
